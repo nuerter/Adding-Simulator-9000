8ab7b77e09207293c37e3047adb5f2e46888a4f3
nuerter <ivan.stanchevv1@gmail.com>
2021-11-29 22:17:05 +0200

    Major update on adding_simulator_9000

---
+++ adding_simulator_9000.py
@@ -0,0 +1,18 @@
+introduction = input("Welcome to \"adding simulator 9000\", press enter to continue.")
+first = input("first digit: ")
+if first == str():
+    print(str("You chose the red pill"))
+    exit()
+else:
+    final_first = float(first)
+second = input("second digit: ")
+if second == str():
+    print(str("You chose the red pill"))
+    exit()
+else:
+    final_second = float(second)
+
+sim = float(final_first + final_second)
+
+if sim != float():
+    print(float(sim))
